3b2c9e0ddcd5b5542129a0833e5d7b31e1c86b16
Mendel <mendelbak@gmail.com>
2018-04-12 17:28:16 -0500

    Added new functionality to Binary Search Tree algorithm (insert vs add methods))

--- algos.js
+++ algos.js
@@ -6,12 +6,12 @@
 // Find sum of all multiples of 3 and 5 from 0 - 1000.
 function multiplesof3and5() {
   var sum = 0;
-  for(var i = 1; i < 1000; i++) {
-    if(i % 3 === 0 || i % 5 === 0) {
+  for (var i = 1; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
   }
-  return(sum);
+  return (sum);
 }
 
 multiplesof3and5(); // Function Call
@@ -24,14 +24,14 @@ function fibSum() {
   var placeholder2 = 1;
   var current = 0;
   var sum = 0;
-  while(current <= 4000000) {
+  while (current <= 4000000) {
     current = placeholder1 + placeholder2;
     placeholder1 = placeholder2;
     placeholder2 = current;
-    if(current % 2 === 0) {
+    if (current % 2 === 0) {
       sum += current;
     }
-    
+
   }
   return sum;
 }
@@ -39,54 +39,103 @@ function fibSum() {
 fibSum(); // Function Call
 
 
-// Create a Binary Search Tree
-// THIS DOES NOT FUNCTION PROPERLY!! NEEDS WORK!
-function BST() {
-  this.root = null;
-  
-  function Node(val) {
-    this.node = val;
-    this.right = null;
-    this.left = null;
+// Create Binary Search Tree and Add New Node. (Equal values insert to the left side).function BST() {
+this.root = null;
+
+function Node(val) {
+  this.value = val;
+  this.right = null;
+  this.left = null;
+}
+
+// Adds a new node to the BST. Does not reorder nodes but appends the new node to the end of the BST following the standard BST chain of logic (placed on right or left side depending on the previous node value compared to new node value).
+
+this.add = function (val) {
+  let root = this.root;
+
+  if (!root) {
+    this.root - new Node(val);
+    return this;
   }
-  
-  this.add = function(val) {
-    var root = this.root;
-    
-    if(!root) {
-      this.root = new Node(val);
-      return this;
-    }
-    
-    var currentNode = this.root;
-    var newNode = new Node(val);
-    
-    while(currentNode) {
-      if(newNode.value <= currentNode.value) {
-        if(!currentNode.left) {
-          currentNode.left = newNode;
-          return this;
+
+  let currentNode = this.root;
+  const newNode = new Node(val);
+
+  while (currentNode) {
+    if (newNode.value <= currentNode.value) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+        return this;
       } else {
         currentNode = currentNode.left;
-        return this;
       }
     }
-    else {
-      if(!currentNode.right) {
-        currentNode.right = newNode;
+    if (newNode.value > currentNode.value) {
+      if (!currentNode.right) {
+        currentNode.right = newnode;
         return this;
       } else {
         currentNode = currentNode.right;
-        return this;
       }
     }
   }
-  return this;
-}
 }
 
-var myBST = new BST();
-myBST.add(13).add(7)add(12);
+// Inserts a new node to the BST while ensuring the node is in the correct position based on its numerical value. (reorders *only* conflicting BST nodes to place new node in proper numerical location).
+this.insert = function (val) {
+  let root = this.root;
+
+  if (!root) {
+    this.root = new Node(val);
+    console.log(`A root has been defined for your BST with a value of ${val}.`);
+    return this;
+  }
+
+  let currentNode = this.root;
+  const newNode = new Node(val);
+
+  while (currentNode) {
+    if (newNode.value <= currentNode.value) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+        return this;
+      } else if (
+        newNode.value <= currentNode.value &&
+        newNode.value > currentNode.left.value
+      ) {
+        let temp = currentNode.left;
+        currentNode.left = newNode;
+        newNode.left = temp;
+        return this;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+    if (newNode.value > currentNode.value) {
+      if (!currentNode.right) {
+        currentNode.right = newNode;
+        return this;
+      } else if (
+        newNode.value > currentNode.value &&
+        newNode.value < currentNode.right.value
+      ) {
+        let temp = currentNode.right;
+        currentNode.right = newNode;
+        newNode.right = temp;
+        return this;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+  }
+  console.log(`A new node with the value of {val} has been inserted in the BST in a position that corresponds to its numerical value.`);
+}
+console.log("A new BST has been created with a null root and no nodes.");
+}
+
+const myBST = new BST();
+
+myBST.insert(7);
 // End BST
 
 
@@ -94,114 +143,110 @@ myBST.add(13).add(7)add(12);
 // Create a Single Linked List 
 function SLL() {
   this.head = null;
-  
-  
+
+
   // Create a Single Linked List.
   function Node(val) {
     this.value = val;
     this.next = null;
   }
-  
+
   // Add a node to the end of a Single Linked List.
-  this.add = function(val) {
-    if(!this.head) {
+  this.add = function (val) {
+    if (!this.head) {
       this.head = new Node(val);
       console.log("You've successfully created a new node.");
-    }
-    else {
+    } else {
       var walker = this.head;
-      while(walker.next) {
+      while (walker.next) {
         walker = walker.next;
       }
       var node = new Node(val);
       walker.next = node;
       console.log("You've successfully created a new node.");
     }
-      return this;
+    return this;
   }
-  
-  
-// Create a function, inside the SLL create function, that checks if the SLL contains a specific value (user submitted).
-  this.SLLContains = function(checkVal) {
+
+
+  // Create a function, inside the SLL create function, that checks if the SLL contains a specific value (user submitted).
+  this.SLLContains = function (checkVal) {
     var walker = this.head;
-    while(walker) {
-      if(walker.value === checkVal) {
+    while (walker) {
+      if (walker.value === checkVal) {
         console.log(`The value ${checkVal} is contained in this Single Linked List`);
         return this;
-      }
-      else {
+      } else {
         console.log(`The value ${checkVal} is not contained in this Single Linked List`);
         return this;
       }
     }
     console.log('You have not passed in a proper Single Linked List into the SLLContains function.');
     return false;
-}
-
-
-// Return the length of a Single Linked List.
-  this.countLength = function() {
-  if(this.head) {
-    var walker = this.head;
-    count = 1;
   }
-  while(walker.next) {
-    count++;
-    walker = walker.next;
+
+
+  // Return the length of a Single Linked List.
+  this.countLength = function () {
+    if (this.head) {
+      var walker = this.head;
+      count = 1;
+    }
+    while (walker.next) {
+      count++;
+      walker = walker.next;
+    }
+    console.log(`You have ${count} node(s) in your Single Linked List`);
+    return this;
   }
-  console.log(`You have ${count} node(s) in your Single Linked List`);
-  return this;
-}
 
 
-// Print all values contained in a Single Linked List.
- this.printVals = function() {
-   var printCounter = 1;
-   if(this.head) {
-     var walker = this.head;
-     console.log(`The value of node #${printCounter} is ${walker.value}`);
-     while(walker.next) {
-       walker = walker.next;
-       printCounter ++
+  // Print all values contained in a Single Linked List.
+  this.printVals = function () {
+    var printCounter = 1;
+    if (this.head) {
+      var walker = this.head;
       console.log(`The value of node #${printCounter} is ${walker.value}`);
-     }
-   }
-   else {
-     console.log('You have not passed in a proper Single Linked List into the printVals function');
-     return undefined;
-   }
-   return this;
- }
+      while (walker.next) {
+        walker = walker.next;
+        printCounter++
+        console.log(`The value of node #${printCounter} is ${walker.value}`);
+      }
+    } else {
+      console.log('You have not passed in a proper Single Linked List into the printVals function');
+      return undefined;
+    }
+    return this;
+  }
 
 
-// Remove a node if it contains a specific, passed in, value.
-  this.remove = function(val) {
-    if(this.head && this.head.next) {
+  // Remove a node if it contains a specific, passed in, value.
+  this.remove = function (val) {
+    if (this.head && this.head.next) {
       prevNode = this.head;
       countNode = this.head.next;
-      if(prevNode.value === val) {
+      if (prevNode.value === val) {
         this.head = countNode;
         prevNode.next = null;
         console.log(`One node, matching your value of ${val}, has been successfully removed.`);
         return this;
       }
-      if(countNode.value === val) {
+      if (countNode.value === val) {
         prevNode.next = countNode.next;
         console.log(`One node, matching your value ${val}, has been successfully removed.`);
         return;
       }
       countNode = countNode.next;
       prevNode = prevNode.next;
+    } else {
+      console.log('No node was removed.');
+      return this;
     }
-    else{
-    console.log('No node was removed.');
-    return this;
-    } 
     return this;
   }
 
-// Bracket below is the ending bracket for the SLL creation function.
-    console.log("Successfully created an empty Single Linked List. It does not yet have a head or nodes.")
+  // Bracket below is the ending bracket for the SLL creation function.
+  console.log("Successfully created an empty Single Linked List. It does not yet have a head or nodes.");
 }
 
 var mySLL = new SLL();
@@ -222,62 +267,61 @@ function rFactorial(num, i = num - 1, newNum = num * i) {
   console.log("num is ->", num);
   console.log("i is ->", i);
   console.log("newNum is ->", newNum);
-if(i < 2) {
-  return newNum;
+  if (i < 2) {
+    return newNum;
+  } else {
+    i--;
+    newNum *= i;
   }
-else{
-  i--;
-  newNum *= i;
-}
-rFactorial(num, i, newNum)
+  rFactorial(num, i, newNum);
 }
 // Function Call
 rFactorial(5);
 
 
 // Recursive function that returns Max, Min, and Avg from a user inputted array. (ES6 syntax)
-function rMaxMinAvg(arr , i = 1, max = arr[0], min = arr[0], sum = arr[0]){
-  if(i >= arr.length){
-    console.log (max, min , sum, sum/arr.length);
+function rMaxMinAvg(arr, i = 1, max = arr[0], min = arr[0], sum = arr[0]) {
+  if (i >= arr.length) {
+    console.log(max, min, sum, sum / arr.length);
     return;
   }
-  if(arr[i] > max) {
-    max = arr[i]; 
+  if (arr[i] > max) {
+    max = arr[i];
   }
-  if(arr[i] < min) {
+  if (arr[i] < min) {
     min = arr[i];
   }
   sum += arr[i];
   rMaxMinAvg(arr, i += 1, max, min, sum);
 }
 // Function Call
-rMaxMinAvg([1,2,3,4,5])
+rMaxMinAvg([1, 2, 3, 4, 5]);
 
 
 // Recursive function that checks a user submitted array for negative numbers. Returns only positive numbers. 
-function rNoNeg(arr, i=0, newArr=[]){
-  if(i>=arr.length){
+function rNoNeg(arr, i = 0, newArr = []) {
+  if (i >= arr.length) {
     console.log(newArr);
     return;
   }
-  if(arr[i] >= 0){
+  if (arr[i] >= 0) {
     newArr.push(arr[i]);
   }
-  rNoNeg(arr, i+=1, newArr);
-  
+  rNoNeg(arr, i += 1, newArr);
+
 }
 // Function Call
-rNoNeg([1,2,-4,8,-30]);
+rNoNeg([1, 2, -4, 8, -30]);
 
 
 
 // Function checks if a passed in number is divisible by 4 and 6. It uses two callbacks to deliver an error or success message.
 function errorHandler(num) {
   if (num % 4 !== 0) {
-    console.log("Num is not divisible by 4!")
+    console.log("Num is not divisible by 4!");
   }
   if (num % 6 !== 0) {
-    console.log("Num is not divisible by 6!")
+    console.log("Num is not divisible by 6!");
   }
 }
 
@@ -299,10 +343,10 @@ function checkDivisibilityBy4and6(num, errorHandler, successHandler) {
   }
 }
 // Function Call
-checkDivisibilityBy4and6(12, errorHandler, successHandler)
+checkDivisibilityBy4and6(12, errorHandler, successHandler);
 
 // Checks a passed in number against all numbers in an array. Will only return a success message if it is divisible by ALL the numbers in the array, else, returns an error message.
-function  checkDivisibilityByArr(num, arr) {
+function checkDivisibilityByArr(num, arr) {
   var error_counter = 0;
   for (var i = 0; i < arr.length; i++) {
     if (num % arr[i] === 0) {
@@ -312,13 +356,13 @@ function  checkDivisibilityByArr(num, arr) {
     }
   }
   if (error_counter <= 0) {
-    return ("Num is divisible by all the values in the array!")
+    return ("Num is divisible by all the values in the array!");
   } else {
-    return ("Num is not divisible by all the values in the array.")
+    return ("Num is not divisible by all the values in the array.");
   }
 }
 // Function Call
-checkDivisibilityByArr(12, [1, 2, 4, 3, 6])
+checkDivisibilityByArr(12, [1, 2, 4, 3, 6]);
 
 
 // Curry function. Takes a  function (add_num) that uses two parameter and returns a new function that executes the callback and uses "ingredient" as the first parameter for that callback function.
@@ -328,7 +372,7 @@ function add_num(x, y) {
 
 function Curry(callback, ingredient) {
   return (y) => {
-    return callback(ingredient, y)
+    return callback(ingredient, y);
   }
 }
 
@@ -339,20 +383,20 @@ add1(10);
 //Return Boolean whether input string is a palindrome or not.
 function isPalindrome(str) {
   var i = 0;
-  for (i = 0; i < str.length/2; i++) {
+  for (i = 0; i < str.length / 2; i++) {
     console.log(str[i]);
     console.log(str[str.length - (i + 1)]);
-    if(str[i] !== str[str.length - (i + 1)]) {
+    if (str[i] !== str[str.length - (i + 1)]) {
       return false;
     }
   }
-return true;
+  return true;
 }
 
 // Function Call
 isPalindrome("racecar"); //function call
 
- 
+
 
 //Return the numerical value (base-10) of an input of Roman numerals.
 function RomeInt(str) {
@@ -385,18 +429,18 @@ RomeInt("MMMMMDCCLXXVIII"); //function call
 function balancePoint(arr) {
   var leftSum = arr[0];
   var rightSum = 0;
-  
-  for(var i = 1; i < arr.length; i++) {
+
+  for (var i = 1; i < arr.length; i++) {
     rightSum += arr[i];
   }
-  if(leftSum === rightSum) {
+  if (leftSum === rightSum) {
     return true;
   }
-  
-  for(var j = 1; j < arr.length - 1; j++) {
+
+  for (var j = 1; j < arr.length - 1; j++) {
     rightSum -= arr[j];
     leftSum += arr[j];
-    if(leftSum === rightSum) {
+    if (leftSum === rightSum) {
       return true;
     }
   }
@@ -404,7 +448,7 @@ function balancePoint(arr) {
 }
 balancePoint([1, 2, 3, 4, 10]); // Function Call
 // function call
-balancePoint([1, 1])
+balancePoint([1, 1]);
 
 //Flatten Array. Given an array that contains arrays inside, remove the items from the subarray and place them in a new array.
 function flattenArr(arr) {
@@ -444,12 +488,12 @@ function reduce(arr, callback) {
   results = callback(arr[0], arr[1]);
   for (let i = 2; i < arr.length; i++) {
 
-    results = callback(arr[i], results)
+    results = callback(arr[i], results);
 
   }
-  return results
+  return results;
 }
-reduce([2, 2, 2, 2], multiply)
+reduce([2, 2, 2, 2], multiply);
 
 
 // Filter Array. Runs a callback on each element in the array, if the result of the callback is false, remove the element from the array.
