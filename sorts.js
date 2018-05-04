9a04aa5c87f10b7a102e61a9a00d105cbbd85b73
Mendel Bak <mendelbak@gmail.com>
2018-05-04 10:16:43 -0500

    Added incomplete Merge Sort

--- sorts.js
+++ sorts.js
@@ -19,3 +19,21 @@ function insertionSort(arr) {
 }
 
 insertionSort([7, 613, 770, 3, 1, 4, -1, -7, 0, 0, 0, 613]);
+
+// Incomplete Merge Sort
+function mergeSort(arr, sort = false) {
+    if (arr.length > 1 && sort === false) {
+        let left = arr.splice(0, Math.floor(arr.length / 2));
+        let right = arr;
+        if (left.length > 1) {
+            left_sort = mergeSort(left);
+        }
+        if (right.length > 1) {
+            left_sort = mergeSort(right);
+        }
+    } else if (arr.length <= 1) {
+        return
+    }
+}
+
+mergeSort([7,1,4,9,2]);
