2e22c0b02d79fce3bbb2effe862ef41679829d78
Mendel Bak <mendelbak@gmail.com>
2018-05-25 10:03:19 -0500

    Formatting.

--- sorts.js
+++ sorts.js
@@ -29,36 +29,38 @@ function mergeSort(arr) {
         left_sort = mergeSort(right);
         return
     }
-    // Function call
-    mergeSort([7, 1, 4, 9, 2]);
+}
+// Function call
+mergeSort([7, 1, 4, 9, 2]);
 
 
-    // Incomplete Quick Sort Function
-    // There are two rules to follow in a Quick Sort function
-    // Rule #1: if(arr[left] >= arr[pivot] && arr[right] <= arr[pivot]) { swap those two values};
-    // Rule #2: if(left >= right) { split the array and recursively call the original quickSort function on both of the new arrays}
+// Incomplete Quick Sort Function
+// There are two rules to follow in a Quick Sort function
+// Rule #1: if(arr[left] >= arr[pivot] && arr[right] <= arr[pivot]) { swap those two values};
+// Rule #2: if(left >= right) { split the array and recursively call the original quickSort function on both of the new arrays}
 
-    function quickSort(arr) {
-        // Variable Declarations
-        let left = arr[0];
-        let right = arr[arr.length - 1];
-        let pivot = arr[Math.floor(arr.length / 2)];
-        let temp;
+function quickSort(arr) {
+    // Variable Declarations
+    let left = arr[0];
+    let right = arr[arr.length - 1];
+    let pivot = arr[Math.floor(arr.length / 2)];
+    let temp;
 
-        // Compare and swap elements that are on the left and right of the pivot point
-        while (left <= right) {
-            if (arr[left] >= arrr[pivot]) {
-                while (right >= left) {
-                    if (arr[right] >= arr[pivot]) {
-                        temp = arr[left];
-                        arr[left] = arr[right];
-                        arr[right] = temp;
-                    }
-                    right--;
+    // Compare and swap elements that are on the left and right of the pivot point
+    while (left <= right) {
+        if (arr[left] >= arrr[pivot]) {
+            while (right >= left) {
+                if (arr[right] >= arr[pivot]) {
+                    temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
                 }
+                right--;
             }
-            left++;
         }
+        left++;
     }
-    // Function Call
-    quickSort([3, 7, 9, 4, 0, 1, 2, 3]);
+}
+
+// Function Call
+quickSort([3, 7, 9, 4, 0, 1, 2, 3]);
